04afe08581f750f06411ab080046e10f0dabe18f
Novemser <novemser@gmail.com>
2024-05-01 09:33:00 +0800

    Staging

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -1138,12 +1138,12 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
         # The output path is set before initialization function is called
         self.prune_metadata = prune_metadata.LlamaPruneMetadata(self, ACT2FN[config.hidden_act], self.prune_metadata.output_path)
-        if config.record_weight_wise_activation:
-            assert self.prune_metadata != None
-            self.prune_metadata.register_hooks_for_layers(self.model.layers)
 
         # Initialize weights and apply final processing
         self.post_init()
+        if config.record_weight_wise_activation:
+            assert self.prune_metadata != None
+            self.prune_metadata.register_hooks_for_layers(self.model.layers)
 
     def get_input_embeddings(self):
         return self.model.embed_tokens
--- src/transformers/prune/prune_metadata.py
+++ src/transformers/prune/prune_metadata.py
@@ -1,16 +1,18 @@
 from .wrapper_layer import WrapperLayer
 from .wrapper_layer import BloomWrapperLayer, LlamaWrapperLayer
-from .sparsity_util import find_layers
+from .sparsity_util import find_layers, sparsify_matrix_for_FC_layer
 import torch
 import os
 
 #PruneMetadata is used to store the statistics during the forward pass of the model.
 class PruneMetadata:
-    def __init__(self, model, output_path=None):
+    def __init__(self, model, output_path=None, enable_weight_wise_pruning=True):
         self.all_wrapper_layers = []
         self.handles = []
         self.model = model
         self.output_path = output_path
+        self.enable_weight_wise_pruning = enable_weight_wise_pruning
+        self.sparsity_percentage = 1.0
 
     def register_hooks_for_layers(self, layers):
         for id, layer in enumerate(layers):
@@ -28,9 +30,27 @@ class PruneMetadata:
                     wrapper_layer.add_batch(inp[0].data, out.data)
                     # print('[DEBUG-1]layer_id:{}, layer_name:{}'.format(layer_id, name))
                 return tmp
+
+            def prune_weights(layer_id, name, wrapper_layer: WrapperLayer):
+                def prune(module, input, output:torch.tensor):
+                    print(layer_id, name, wrapper_layer.layer_name, self.output_path)
+                    _, seq_len, d_model = output.size()
+                    filename = f"{layer_id}_{name}.pt"
+                    # Load the activations from previously recorded files
+                    res = torch.load(os.path.join(self.output_path, filename))
+                    assert res.size() == output.size()
+                    pruned_res = sparsify_matrix_for_FC_layer(res, self.sparsity_percentage, seq_len * d_model) > 0
+                    return output * pruned_res
+                    
+                return prune
             for name, wrapper_layer in wrapper_layers.items():
-                self.handles.append(subset[name].register_forward_hook(add_batch(id, name, wrapper_layer)))
-    
+                if self.enable_weight_wise_pruning:
+                    # prune weight based on recorded activation information
+                    self.handles.append(subset[name].register_forward_hook(prune_weights(id, name, wrapper_layer)))
+                else:
+                    # record activation information
+                    self.handles.append(subset[name].register_forward_hook(add_batch(id, name, wrapper_layer)))                
+
     def find_instrument_layers(self, layer):
         return find_layers(layer)
     
