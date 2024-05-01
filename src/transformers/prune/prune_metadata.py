3fee10aabf6e4ce316485ce9989361cc29492532
Gansen <novemser@gamil.com>
2024-05-01 10:38:10 +0800

    Fix weight-wise pruning

--- src/transformers/prune/prune_metadata.py
+++ src/transformers/prune/prune_metadata.py
@@ -1,18 +1,40 @@
 from .wrapper_layer import WrapperLayer
 from .wrapper_layer import BloomWrapperLayer, LlamaWrapperLayer
-from .sparsity_util import find_layers, sparsify_matrix_for_FC_layer
+from .sparsity_util import find_layers
 import torch
+from torch import nn
 import os
+
+def prune_by_weight_importance(
+        weight: torch.tensor,
+        weight_importance: torch.tensor,
+        pruning_percentage,
+        layer_wise_weight_selection=False) -> torch.tensor:
+    original_size = weight.size()
+    topk = int(pruning_percentage * weight_importance.reduce())
+    preserved_indices = weight.view(-1,).topk(topk).indices
+    mask = torch.zeros_like(weight).scatter(0, preserved_indices, 1).bool().half()
+    return weight.view(original_size) * mask
+
+def prune_by_column_importance(weight: torch.tensor, weight_importance: torch.tensor) -> torch.tensor:
+    pass
+
+PRUNING_FUNC_MAP = {
+    'weight', prune_by_weight_importance,
+    'column', prune_by_column_importance
+}
 
 #PruneMetadata is used to store the statistics during the forward pass of the model.
 class PruneMetadata:
-    def __init__(self, model, output_path=None, enable_weight_wise_pruning=True):
+    def __init__(self, model, output_path=None, enable_weight_wise_pruning=True, pruning_strategy='weight'):
         self.all_wrapper_layers = []
         self.handles = []
         self.model = model
         self.output_path = output_path
         self.enable_weight_wise_pruning = enable_weight_wise_pruning
         self.sparsity_percentage = 1.0
+        assert pruning_strategy in PRUNING_FUNC_MAP
+        self.pruning_func = PRUNING_FUNC_MAP[pruning_strategy]
 
     def register_hooks_for_layers(self, layers):
         for id, layer in enumerate(layers):
@@ -31,22 +53,14 @@ class PruneMetadata:
                     # print('[DEBUG-1]layer_id:{}, layer_name:{}'.format(layer_id, name))
                 return tmp
 
-            def prune_weights(layer_id, name, wrapper_layer: WrapperLayer):
-                def prune(module, input, output:torch.tensor):
-                    print(layer_id, name, wrapper_layer.layer_name, self.output_path)
-                    _, seq_len, d_model = output.size()
-                    filename = f"{layer_id}_{name}.pt"
-                    # Load the activations from previously recorded files
-                    res = torch.load(os.path.join(self.output_path, filename))
-                    assert res.size() == output.size()
-                    pruned_res = sparsify_matrix_for_FC_layer(res, self.sparsity_percentage, seq_len * d_model) > 0
-                    return output * pruned_res
-                    
-                return prune
             for name, wrapper_layer in wrapper_layers.items():
                 if self.enable_weight_wise_pruning:
+                    module = subset[name]
+                    filename = f"{id}_{name}.pt"
+                    # Load the activations from previously recorded files
+                    activation_info = torch.load(os.path.join(self.output_path, filename))
                     # prune weight based on recorded activation information
-                    self.handles.append(subset[name].register_forward_hook(prune_weights(id, name, wrapper_layer)))
+                    module.weight = nn.Parameter(self.pruning_func(module.weight, activation_info, self.sparsity_percentage))
                 else:
                     # record activation information
                     self.handles.append(subset[name].register_forward_hook(add_batch(id, name, wrapper_layer)))                
